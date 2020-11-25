4a90bdec64320d5cb496a0063e7eb7dda269bc95
pianocomposer321 <tmarklebw88@gmail.com>
2020-11-24 21:50:10 -0600

    Updated install.sh

--- install.bash
+++ install.bash
@@ -10,4 +10,6 @@ config checkout || (mv .bashrc .bashrc~; mv .config/init.vim .config/init.vim~; 
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+config checkout
+config config --local status.showUntrackedFiles no
 nvim +PlugInstall +qall
