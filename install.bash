e979a867cba0e5ba810abb9b21d840bc281b4bb9
pianocomposer321 <tmarklebw88@gmail.com>
2020-11-24 21:47:44 -0600

    Initial commit

---
+++ install.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function config {
+    /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME
+}
+
+echo ".dotfiles" >> .gitignore
+git clone --bare https://github.com/pianocomposer321/dotfiles-installer
+config checkout || (mv .bashrc .bashrc~; mv .config/init.vim .config/init.vim~; mv .tmux.conf .tmux.conf~; mv .tmux .tmux~)
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim +PlugInstall +qall
