851090a3f61459a8e43c5e372e36cd75e68c8a2b
pianocomposer321 <tmarklebw88@gmail.com>
2020-11-28 06:18:28 -0500

    Updated install.bash

--- install.bash
+++ install.bash
@@ -7,7 +7,8 @@ alias config="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/pianocomposer321/dotfiles $HOME/.dotfiles
-config checkout || (mv .bashrc .bashrc~; mv $HOME/.config/init.vim $HOME/.config/init.vim~; mv $HOME/.tmux.conf $HOME/.tmux.conf~; mv $HOME/.tmux $HOME/.tmux~)
+config checkout || (mv $HOME/.bashrc $HOME/.bashrc~; mv $HOME/.config/init.vim $HOME/.config/init.vim~; mv $HOME/.tmux.conf $HOME/.tmux.conf~; mv $HOME/.tmux $HOME/.tmux~; config checkout)
+mkdir $HOME/.local/share/nvim/backup
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
