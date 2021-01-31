048e7b73bd2bddae70468fe58d2e1047b6cda1f7
pianocomposer321 <54072354+pianocomposer321@users.noreply.github.com>
2021-01-30 19:05:00 -0600

    Update install.bash
    Fix alias problem

--- install.bash
+++ install.bash
@@ -3,6 +3,7 @@
 # function config {
 #     /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME
 # }
+shopt -s expand_aliases
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
 echo ".dotfiles" >> .gitignore
