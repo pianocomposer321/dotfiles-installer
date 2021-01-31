814e327bfd1cad86d45126ec4d084185edea5e72
pianocomposer321 <tmarklebw88@gmail.com>
2021-01-30 19:17:22 -0600

    Fixed error in install.bash

--- install.bash
+++ install.bash
@@ -9,9 +9,7 @@ alias config="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/pianocomposer321/dotfiles $HOME/.dotfiles
 config checkout || (mv $HOME/.bashrc $HOME/.bashrc~; mv $HOME/.config/init.vim $HOME/.config/init.vim~; mv $HOME/.tmux.conf $HOME/.tmux.conf~; mv $HOME/.tmux $HOME/.tmux~; config checkout)
-mkdir $HOME/.local/share/nvim/backup
+mkdir -p $HOME/.local/share/nvim/backup
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 config config --local status.showUntrackedFiles no
-nvim +PlugInstall +qall
+nvim -u install.lua
